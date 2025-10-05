e9226c7071c3e09efc3cfd77b6e09ddd1fc97492
julia-midon <julia.midon@polijunior.com.br>
2025-10-05 19:27:33 -0300

    estilizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
-// Código JS aqui!
+const botao = document.getElementById('produtoBtn');
+
+function mudarTextoDoBotao() {
+    if (botao.innerText === 'Adicionar ao carrinho') {
+        botao.innerText = 'Remover do carrinho';
+        botao.style.backgroundColor = '#f73b23ff'; 
+    } else if (botao.innerText === 'Remover do carrinho'){
+        botao.innerText = 'Adicionar ao carrinho';
+        botao.style.backgroundColor = '#ffffffff';
+    }
+}
+
+if (botao){
+botao.addEventListener('click', mudarTextoDoBotao);
+};
