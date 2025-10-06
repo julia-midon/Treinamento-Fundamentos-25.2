1b29d567562855651882a3861b95b1d15e4ef77e
julia-midon <julia.midon@polijunior.com.br>
2025-10-05 21:30:31 -0300

    finalizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,45 @@
-const botao = document.getElementById('produtoBtn');
+document.addEventListener("DOMContentLoaded", function() {
+  const botoes = document.querySelectorAll(".main");
+  const contador = document.getElementById("contador");
+  let contadorCarrinho = 0;
 
-function mudarTextoDoBotao() {
-    if (botao.innerText === 'Adicionar ao carrinho') {
-        botao.innerText = 'Remover do carrinho';
-        botao.style.backgroundColor = '#f73b23ff'; 
-    } else if (botao.innerText === 'Remover do carrinho'){
-        botao.innerText = 'Adicionar ao carrinho';
-        botao.style.backgroundColor = '#ffffffff';
-    }
-}
+  botoes.forEach(function(botao) {
+    
+    botao.addEventListener("click", function() {
+      if (botao.textContent === "Adicionar ao carrinho") {
+        botao.textContent = "Remover do carrinho";
+        botao.style.backgroundColor = "coral";   
+        botao.classList.add("adicionado");
+        contadorCarrinho++; 
+      } else {
+        botao.textContent = "Adicionar ao carrinho";
+        botao.style.backgroundColor = "white";
+        botao.classList.remove("adicionado");
+        contadorCarrinho--;
+      }
+            contador.textContent = ": " + contadorCarrinho;
 
-if (botao){
-botao.addEventListener('click', mudarTextoDoBotao);
-};
+    });
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const capa = document.getElementById("capa");
+  const entrarBtn = document.getElementById("entrarBtn");
+  const nomeInput = document.getElementById("nomeUsuario");
+
+  entrarBtn.addEventListener("click", function() {
+    const nome = nomeInput.value.trim();
+    
+    const usuario = document.querySelector(".direita h4");
+    usuario.textContent = `${nome} |`;
+
+    capa.style.opacity = "0";
+    setTimeout(() => {
+      capa.style.display = "none";
+    }, 600);
+  });
+});
+
+
